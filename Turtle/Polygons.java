872516ccd103354c73f398cbaf97f1f1517fcd46
Samir Chowdhury <43937499+samirchowd@users.noreply.github.com>
2019-09-09 15:38:27 -0400

    Changes to be committed: 	modified:   Polygons.class 	modified:   Polygons.java
     Untracked files:
    	DashedPoly.class
    	DashedPoly.java

--- Turtle/Polygons.java
+++ Turtle/Polygons.java
@@ -1,6 +1,6 @@
 public class Polygons
 {
-	// Draws an n-polygon taking in a turtle and n
+	// Draws an n-polygon taking in a turtle and a size
 	public static void DrawPoly(Turtle t, int n, double size)
 	{
 		int deg = 360/n; 
@@ -12,6 +12,7 @@ public class Polygons
 		}
 	}	
 
+	// Moves a turtle t forward n units without drawing 
 	public static void Move(Turtle t, int n)
 	{
 		t.penup();
@@ -21,9 +22,7 @@ public class Polygons
 
 	public static void main(String[] args)
 	{
-		Turtle t = new Turtle();
-		DrawPoly(t, 5, 50);
-		Move(t, 200);
-		DrawPoly(t,9,70);
+		Turtle t = new Turtle(); 
+		DashLine(t, 5, 100);
 	}
 }
