96b3879bcb86aebbbe2b44cd3bc662f1309947cd
Samir Chowdhury <43937499+samirchowd@users.noreply.github.com>
2019-09-16 11:53:02 -0400

    Changes to HW 1 as of 11:52 AM 9/16/19

--- HW1/prob1.java
+++ HW1/prob1.java
@@ -3,12 +3,18 @@ public class prob1
 {
 	/*
 	Methods needed to be written 
-	1. Method to draw an n-polygon 
-	2. Method to draw
+	1. Method to draw an n-polygon X
+	2. Method to draw columns X
+	3. Method to draw a brick wall X
+	4. Method to draw arches
+	5. Method to draw trees
+	7. Method to draw window arrays
+	8. Method to draw stars
 	*/
 
 
-	// Draws an n-polygon of a certain size
+	// Draws an n-sided polygon. Takes in a a turtle t, the number of sides n,
+	// and the size of each side size.
 	public static void DrawPoly(Turtle t, int n, double size)
 	{
 		int deg = 360/n; 
@@ -19,6 +25,13 @@ public class prob1
 			t.left(deg);
 		}
 	}	
+
+	public static void Move(Turtle t, int n)
+	{
+		t.penup();
+		t.forward(n);
+		t.pendown();
+	}
 
 	// Draws a castle column with turtle t 
 	public static void Column(Turtle t)
@@ -55,30 +68,230 @@ public class prob1
 		t.forward(100);
 		t.right(90);
 		t.forward(50);
+
+		t.right(180);
+		Move(t, 10);
+		t.left(90);
+		Move(t, 80);
+		t.right(90);
+
+		Windows(t);
 	}
 
+	/*
+	Method to draw first variation of brick wall taking in Turtle t as an input
+	*/
+	public static void Brick1(Turtle t)
+	{
+		//For loop to draw the line of bricks
+		for(int j = 0; j < 8; j++)
+			{
+				for(int k = 0; k < 2; k++)
+				{
+					t.forward(10);
+					t.left(90);
+					t.forward(5);
+					t.left(90);
+				}
+				t.forward(10);
+			}
+	}
+	
+	/*
+	Method to draw second variation of brick wall taking in Turtle t as an input
+	*/
+	public static void Brick2(Turtle t)
+	{
+		// For loop for the entire line
+		for(int i = 0; i < 9; i++)
+		{
+			if(i == 0 || i == 8)// First and last brick are unique
+			{
+				for(int j = 0; j < 4; j++)
+				{
+					t.forward(5);
+					t.left(90);
+				}
+				t.forward(5);
+			}
+			else
+			{
+				for(int k = 0; k < 2; k++)//For loop to draw the rest of the bricks
+				{
+					t.forward(10);
+					t.left(90);
+					t.forward(5);
+					t.left(90);
+				}
+				t.forward(10);
+			}
+		}
+	}
+
+	/*
+	Method that combines Brick1 & Brick2 to form the entire brick wall taking Turtle
+	t as an input
+	*/
 	public static void Brick(Turtle t)
 	{
-		//For loop for the entire wall
+		//For loop for the entire wall, uses variation 2 on odd levels
 		for(int i = 0; i < 7; i++)
 		{
-			
-		
+			if(i%2 != 0)
+			{
+				Brick2(t);
+			}
+			else
+			{
+				Brick1(t);
+			}
+			t.backward(80);
+			t.left(90);
+			t.forward(5);
+			t.right(90);
+		}
+	}
+
+	public static void Windows(Turtle t)
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 2; j++)
+			{
+				DrawPoly(t,4,10);
+				Move(t,20);
+			}
+			t.left(180);
+			Move(t,40);
+			t.left(90);
+			Move(t,20);
+			t.left(90);
+		}
+	}
+
+
+	public static void Arch(Turtle t)
+	{
+		t.forward(30);
+
+		for(int i = 0; i < 30; i++)
+		{
+			t.forward((2*Math.PI*20)/60);
+			t.right(360.0/60);
+		}
+
+		t.forward(35);
+		t.right(90);
+		t.forward(10);
+		t.right(90);
+		t.forward(30);
+
+		for(int i = 0; i < 30; i++)
+		{
+			t.forward((2*Math.PI*10)/60);
+			t.left(360.0/60);
+		}
+		t.forward(30);
+		t.right(90);
+		t.forward(10);
+		t.right(180);
+
+	}
+
+	public static void Star(Turtle t)
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			t.forward(10);
+			t.backward(20);
+			t.forward(10);
+			t.right(45);
+		}
+		t.right(135);
+	}
+
+	public static void Tree(Turtle t)
+	{
+		t.forward(20);
+		for(int i = 0; i < 5; i++)
+		{
+			t.right(135);
+			t.forward(Math.sqrt(200));
+			t.left(180);
+			t.forward(Math.sqrt(200));
+			t.left(90);
+			t.forward(Math.sqrt(200));
+			t.backward(Math.sqrt(200));
+			t.right(135);
+			if(i == 4)
+			{
+				Move(t,5);
+			}
+			else
+			{
+				t.forward(5);
+			}
 
 		}
-	}	
-
+		t.penup();
+		t.backward(40);
+		t.pendown();
+	}
 
 	public static void main(String[] args)
 	{
 		Turtle t = new Turtle();
+		
+		//Drawing column 1
 		t.left(90);
-		Column(t);
-		t.right(180);
 
-		t.forward(50);
+		Column(t);
+
+		t.right(90);
+		Move(t,20);
+		t.left(90);
+		Move(t,40);
 
 		Brick(t);
+
+		t.right(90);
+		Move(t, 35);
+		t.left(90);
+		Move(t,80);
+		t.left(90);
+
+		Arch(t);
+
+		Move(t,40);
+		t.left(90);
+
+		Column(t);
+
+		t.right(90);
+		Move(t,20);
+		t.left(90);
+		Move(t,40);
+
+		Move(t,20);
+		t.left(90);
+		Move(t,30);
+
+		for(int i = 0; i < 3; i++)
+		{
+			Arch(t);
+			Move(t,60);
+			t.left(90);
+			Move(t,20);
+
+		}
+
+		t.left(180);
+		for(int i = 0; i < 3; i++)
+		{
+			Move(t,20);
+			t.right(90);
+			Move(t,60);
+		}
 	}
 
 
